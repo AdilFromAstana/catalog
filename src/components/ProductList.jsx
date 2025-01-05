43b3ea795f813086fc548ae6c611e22111afb511
Adil Aizhanov <adilfirstus@gmail.com>
2025-01-05 09:33:15 +0500

    d

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,41 +1,49 @@
 import { List } from "antd";
 import { HeartOutlined, HeartFilled } from "@ant-design/icons";
 import { Link } from "react-router-dom";
+import useWindowWidth from "../hooks/useWindowWidth";
 
-const ProductList = ({ products, favorites, toggleFavorite }) => (
-  <List
-    dataSource={products}
-    renderItem={(item) => (
-      <List.Item className="list-item">
-        <img
-          src={item.previewImages[0].small}
-          alt={item.previewImages[0].small}
-          className="product-image"
-        />
-        <div className="product-details">
-          <div className="product-info">
-            <Link className="product-title" to={`/${item.id}`}>
-              {item.title}
-            </Link>
-            <div className="product-category">
-              {item.category[item.category.length - 1]}
+const ProductList = ({ products, favorites, toggleFavorite }) => {
+  const width = useWindowWidth();
+  console.log("width: ", width);
+  return (
+    <List
+      grid={{
+        column: width > 429 ? 2 : 1,
+      }}
+      dataSource={products}
+      renderItem={(item) => (
+        <List.Item className="list-item">
+          <img
+            src={item.previewImages[0].small}
+            alt={item.previewImages[0].small}
+            className="product-image"
+          />
+          <div className="product-details">
+            <div className="product-info">
+              <Link className="product-title" to={`/${item.id}`}>
+                {item.title}
+              </Link>
+              <div className="product-category">
+                {item.category[item.category.length - 1]}
+              </div>
+              <div className="product-price">{item.priceFormatted}</div>
             </div>
-            <div className="product-price">{item.priceFormatted}</div>
+            <div
+              className="product-favorite"
+              onClick={() => toggleFavorite(item.id)}
+            >
+              {favorites.includes(item.id) ? (
+                <HeartFilled style={{ color: "red", fontSize: "24px" }} />
+              ) : (
+                <HeartOutlined style={{ fontSize: "24px" }} />
+              )}
+            </div>
           </div>
-          <div
-            onClick={() => toggleFavorite(item.id)}
-            style={{ cursor: "pointer" }}
-          >
-            {favorites.includes(item.id) ? (
-              <HeartFilled style={{ color: "red", fontSize: "24px" }} />
-            ) : (
-              <HeartOutlined style={{ fontSize: "24px" }} />
-            )}
-          </div>
-        </div>
-      </List.Item>
-    )}
-  />
-);
+        </List.Item>
+      )}
+    />
+  );
+};
 
 export default ProductList;
